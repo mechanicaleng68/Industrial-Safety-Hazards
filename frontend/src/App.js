9693a8aefe65292cc64b33c70fa888106396a1f1
Mariela <mvmihilova@gmail.com>
2022-11-15 11:00:16 -0700

    added textArea btns:Submit, Upload, Address, TODOs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,47 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return; 
-    <div className="App"></div>;
+function App () {
+  return (
+    <div className="App">
+
+      <header className="app-header">
+        <h1>Safety Hazards</h1>
+      </header>
+
+
+{/*TODO: Need to connect safety hazard description, provided by user to collect in MongoDB;*/} 
+      <textarea className="description" /> 
+      
+
+{/*TODO: Need to make button clickable and fix .btn-submit at App.css- something is wrong with the syntax */}
+      <button type="submit">Submit</button>
+      
+
+{/*TODO: Add relevant feature for upload photo, upload to BLOB- Amazon than connect to MONGODB*/}      
+      <button
+        style={{
+          backgroundColor: 'orange',
+          padding: '10px',
+          borderRadius: '10px',
+        }}
+      >
+        Upload photo
+      </button>
+
+{/*TODO: Add relevant feature for user to add address which will be converted to geolocation*/}
+      <button
+        style={{
+          backgroundColor: 'orange',
+          padding: '10px',
+          borderRadius: '10px',
+        }}
+      >
+        Add Address
+      </button>
+    </div>
+  
+  );
 }
 
 export default App;
