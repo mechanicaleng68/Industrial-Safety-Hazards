45f03798e8d92f12e32da17e72721aeea9847635
Mariela <mvmihilova@gmail.com>
2022-11-20 00:28:26 -0700

    can view uploaded photo

--- frontend/src/ImageUpload.js
+++ frontend/src/ImageUpload.js
@@ -1,15 +1,38 @@
-import React, { useState } from "react";
+import React, {useState} from 'react';
 import './ImageUpload.css';
-function ImageUpload(){
-    const [data,setData]=useState();
-    console.log(data)
-    return(
-        <div className="main">
-            <div className="upload-image">
-                <label htmlFor="imgs">Upload Photo</label>
-            </div>
-            <input id="imgs" type="file" accept="image/png, image/jpeg,.txt,.doc" onChange={(e)=>setData(e.target.files)} />
-        </div>
-    );
+
+function ImageUpload () {
+  const [file, setFile] = useState ();
+  console.log (file);
+
+  function handleChange (e) {
+    console.log (e.target.files);
+    setFile (URL.createObjectURL (e.target.files[0]));
+  }
+
+  return (
+    <div className="main">
+      <div className="upload-image">
+        <label htmlFor="imgs">Add Photo</label>
+      </div>
+      <input
+        id="imgs"
+        type="file"
+        accept="image/png, image/jpeg,.txt,.doc"
+        onChange={handleChange}
+      />
+      <img
+        src={file}
+        style={{
+          height: 150,
+          width: 270,
+          borderRadius: '10%',
+          border: '1px orange',
+        }}
+        alt="Safety hazard"
+      />
+
+    </div>
+  );
 }
 export default ImageUpload;
