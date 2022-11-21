988e0af016a7bf9e6829ec1852342f67763cb580
Reza Nemati <ranemati47@gmail.com>
2022-11-21 01:22:03 -0700

    created onnection to server of frontend to backend and database

---
+++ frontend/src/components/HazardForm.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+const HazardForm = () => {
+  const [hazardName, setHazardName] = useState("");
+  const [hazardDescription, setHazardDescription] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const hazard = {
+        hazardName,
+        hazardDescription,
+    };
+    const response = await fetch("/api/hazard", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(hazard),
+    });
+    const newHazard = await response.json();
+    console.log(newHazard);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Hazard Name:
+        <input onChange={(event) => setHazardName(event.target.value)} value={hazardName} />
+      </label>
+      <label>
+        Hazard Description:
+        <input
+          onChange={(event) => setHazardDescription(event.target.value)}
+          value={hazardDescription}
+        />
+      </label>
+      <button type="submit">Add Hazard</button>
+    </form>
+  );
+};
+
+export default HazardForm;
---
+++ frontend/src/components/HazardLists.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+const HazardDetail = (props) => {
+  const { hazardName, hazardDescription, hazardPostalCode, hazardDate, hazardLocation } = hazard;
+  return (
+    <div>
+      <p>Hazard Name:</p>
+      <p>{hazardName}</p>
+      <p>Hazard Description:</p>
+      <p>{hazardDescription}</p>
+      <p>Hazard Postal Code:</p>
+      <ul>
+        {hazardPostalCode.map((power) => {
+          return <li key={hazardPostalCode}>{hazardPostalCode}</li>;
+        })}
+      </ul>
+      <p>{hazardDate}</p>
+      <p>Location:</p>
+      {!hazardLocation ? (
+        <p>Unknown</p>
+      ) : (
+        <div>
+          <p>City:</p>
+          <p>{hazardLocation.city}</p>
+          <p>Province:</p>
+          <p>{hazardLocation.province}</p>
+          <p>Country:</p>
+          <p>{hazardLocation.country}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+const HazardList = () => {
+  const [hazards, setHazards] = useState([]);
+  useEffect(() => {
+    const getHazards = async () => {
+      const response = await fetch("/api/hazard/list");
+      const hazardsData = await response.json();
+      setHazards(hazardsData);
+    };
+    getHazards();
+  }, []);
+  return (
+    <div>
+      <h2>Hazard List</h2>
+      {hazards.length > 0
+        ? hazards.map((hazard) => {
+            return <HazardDetail hazard={hazard} />;
+          })
+        : "No hazards"}
+    </div>
+  );
+};
+
+export default HazardList;
