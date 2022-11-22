e6ee1dd71ea6b50d50b207c78199cf524df01bd4
Mariela <mvmihilova@gmail.com>
2022-11-22 00:40:18 -0700

    nav bar- home

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,14 @@
+import '../App.css';
+export default function Home() {
+    return (
+       
+    <div
+    style={{
+      backgroundImage: `url("https://noobpreneur.com/wp-content/uploads/2015/04/funny-workplace-safety-sign.jpg")`,
+
+    }}
+  >
+    JUST A BACKGROUND TEST
+  </div>
+    );
+}
