467175c877928038180d000ca86b46bf217c36ef
Mariela <mvmihilova@gmail.com>
2022-11-22 00:48:37 -0700

    added Navigation bar and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import {useState} from 'react';
+import {Route, Routes} from 'react-router-dom';
 import './App.css';
+import Navigation from './Navigation';
+import Home from './pages/Home';
+import About from './pages/About';
+import AddNew from './pages/AddNew';
 
-import  HorizontalLinearStepper from './Stepper.js';
+//import HorizontalLinearStepper from './Stepper.js';
 
-
-import ImageUpload from './ImageUpload';
+import Footer from './Footer';
 
 function App () {
-  const [state, setState] = useState ('add description');
-
- /*console.log('before useEffect');
+  /*console.log('before useEffect');
   useEffect (
     () => {
       setState(TODO: API to be added, see Dave Gray tutorial)
@@ -23,46 +24,17 @@ function App () {
     <React.Fragment>
       {/*React.Fragment is used to stack divs*/}
 
-      <div className="App">
-        <header className="app-header">
-          <h1>Safety Hazards</h1>
-        </header>
+      <Navigation />
+
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<AddNew />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
       </div>
 
-{/*Currently working on adding Stepper from Material UI */}
-
-      <div>
-        <h2>How to submit a safety hazard warning:</h2>
-        <HorizontalLinearStepper />
-        <ol>
-          <li>Provide a description of the safety hazard.</li>
-          <li>Provide the address of the safety hazard.</li>
-          <li>Upload a photo (optional)</li>
-          <li>Submit</li>
-        </ol>
-      </div>
-
-      <div>
-        {/*TODO: Need to connect safety hazard description, provided by user to collect in MongoDB;*/}
-        <textarea
-          className="description"
-          value={state}
-          onChange={e => setState (e.target.value)}
-        />
-
-        <ImageUpload />
-
-      </div>
-
-      <div>
-        <button
-          onClick={() => console.log (state, File)}
-          className="btn-submit"
-        >
-          {' '}
-          Submit
-        </button>
-      </div>
+      <Footer />
     </React.Fragment>
   );
 }
