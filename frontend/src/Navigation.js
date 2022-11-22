b610f6c8a9b1b879f396fddccfe6f5f442263b8b
Mariela <mvmihilova@gmail.com>
2022-11-22 00:50:09 -0700

    added navigation bar

---
+++ frontend/src/Navigation.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Link, useMatch, useResolvedPath} from 'react-router-dom';
+
+export default function Navigation () {
+  return (
+    <nav className="nav">
+      <Link to="/" className="site-title">
+      Safety Hazards
+      </Link>
+      <ul>
+        <CustomLInk to="/new">Add New</CustomLInk>
+        <CustomLInk to="/about">About</CustomLInk>
+
+      </ul>
+    </nav>
+  );
+}
+
+function CustomLInk({to, children, ...props}) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({path: resolvedPath.pathname, end: true});
+  return (
+    <li className={isActive === to ? 'active' : ''}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  );
+}
