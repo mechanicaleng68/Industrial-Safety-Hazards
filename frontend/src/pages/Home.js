ec5a710dc21940aacab328629833c911a2de6e5c
Mariela <mvmihilova@gmail.com>
2022-11-22 14:52:48 -0700

    updated home page height to 85vh

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,13 +1,14 @@
-import { height } from '@mui/system';
 import React from 'react';
 
-function Home() {
+function Home () {
   return (
-    <div style={{ 
-      backgroundImage: `url("https://noobpreneur.com/wp-content/uploads/2015/04/funny-workplace-safety-sign.jpg")`,
-      backgroundSize: "cover",
-      height: "100vh",
-   }}>
+    <div
+      style={{
+        backgroundImage: `url("https://noobpreneur.com/wp-content/uploads/2015/04/funny-workplace-safety-sign.jpg")`,
+        backgroundSize: 'cover',
+        height: '85vh',
+      }}
+    >
       Hello World
     </div>
   );
