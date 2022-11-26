a251ffefd91bdc69a3793e3d8e8942f5d0044e52
Mariela <mvmihilova@gmail.com>
2022-11-25 21:44:00 -0700

    added user name and phone to Schema

--- backend/db/models/hazardModel.js
+++ backend/db/models/hazardModel.js
@@ -2,30 +2,31 @@ import mongoose from '../mongoose.js';
 
 const Schema = mongoose.Schema;
 
-const hazardSchema = new Schema({
-  hazardName: { type: String, required: true, unique: true },
-  hazardPostalCode: [String],
-  hazardDescription: [String],
-  hazardDate: [{ Day: Number, Mont: Number, Year: Number }],
+const hazardSchema = new Schema ({
+  userName: {type: String, required: true, unique: true},
+  userPhone: {type: String, required: true, unique: true},
+  hazardPostalCode: {type: String, required: true, unique: true},
+  hazardDescription: {type: String, required: true, unique: true},
+  hazardDate: [{Day: Number, Month: Number, Year: Number}],
   hazardLocation: {
     type: {
       city: String,
       province: String,
       country: String,
-      postalCode: String
+      postalCode: String,
     },
-    required: false
-  }
+    required: false,
+  },
 });
 
-const Hazard = mongoose.model('hazard', hazardSchema, 'hazards');
+const Hazard = mongoose.model ('hazard', hazardSchema, 'hazards');
 
 export const getAllHazards = async () => {
-  const Hazards = await Hazard.find();
+  const Hazards = await Hazard.find ();
   return Hazards;
 };
 
-export const createHazard = async (hazard) => {
-  const newHazard = await Hazard.create(hazard);
+export const createHazard = async hazard => {
+  const newHazard = await Hazard.create (hazard);
   return newHazard;
 };
