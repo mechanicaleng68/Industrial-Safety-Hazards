d8f61328aedd8ce0d08ef801e146dec4ce8ed0c4
Mariela <mvmihilova@gmail.com>
2022-11-26 16:19:57 -0700

    moved style to App.css

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,14 +3,9 @@ import React from 'react';
 function Home () {
   return (
     <img
-      className="home-image"
+      className="home-image-bird"
       src="https://noobpreneur.com/wp-content/uploads/2015/04/funny-workplace-safety-sign.jpg&quot;)"
       alt="bird"
-      style={{
-        backgroundSize: 'cover',
-        height: '75vh',
-        width: '80vw',
-      }}
     />
   );
 }
