2c85b1e91a7bf1a6f0198c8427ecc20b9da359d2
Mariela <mvmihilova@gmail.com>
2022-11-26 21:23:05 -0700

    added border

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -8,6 +8,7 @@ export default function About () {
       <div>
         <h1>About</h1>;
         <p>This website was created for learning purposes only!</p>
+        <p />
         <p>
           Any resemblance to actual events or persons, living or dead, is entirely coincidental!
         </p>
@@ -20,13 +21,18 @@ export default function About () {
         <h2>"Guilty" for this to happen:</h2>
         <h3>"Smokin' RMD" Team:</h3>
         <img
+          className="photo-smokin-rmd"
           src="https://firebasestorage.googleapis.com/v0/b/project-2-c8133.appspot.com/o/Mariela_resized.png?alt=media&token=2685364b-fd7b-470e-9042-2ca4cea6c7f0"
           alt="Mariela"
         />
+        Mariela
         <img
+          className="photo-smokin-rmd"
           src="https://firebasestorage.googleapis.com/v0/b/project-2-c8133.appspot.com/o/Reza_resized.png?alt=media&token=22302c3f-7687-40bb-8ea1-1852840013f8"
           alt="Reza"
         />
+        {' '}
+        Reza
 
       </div>
     </React.Fragment>
