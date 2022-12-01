c0ae50edc4aa45c03b6b57cc62a87d4c5d552b0e
Reza Nemati <ranemati47@gmail.com>
2022-12-01 00:36:43 -0700

    change in return string

--- backend/routes/hazardRoutes.js
+++ backend/routes/hazardRoutes.js
@@ -1,48 +1,48 @@
-import {Router} from 'express';
+import { Router } from 'express';
 import {
   createHazard,
   getAllHazards,
-  getHazardById,
+  getHazardById
 } from '../db/models/hazardModel.js';
-import {debug} from '../server.js';
+import { debug } from '../server.js';
 
-const router = Router ();
+const router = Router();
 
-router.get ('/', async (req, res) => {
+router.get('/', async (req, res) => {
   //const hazard = req.body;
-  debug ('in list route');
+  debug('in list route');
   try {
-    const hazards = await getAllHazards ();
-    res.send (hazards);
+    const hazards = await getAllHazards();
+    res.send(hazards);
   } catch (error) {
-    debug (error);
-    res.status (500).send (error);
+    debug(error);
+    res.status(500).send(error);
   }
 });
 
-router.post ('/', async (req, res) => {
+router.post('/', async (req, res) => {
   const hazard = req.body;
-  debug ('in create route', hazard);
+  debug('in create route', hazard);
   try {
-    const newHazard = await createHazard (hazard);
-    res.send (newHazard);
+    const newHazard = await createHazard(hazard);
+    res.send(newHazard);
   } catch (error) {
-    debug (error);
-    res.status (500).send (error);
+    debug(error);
+    res.status(500).send(error);
   }
 });
 
-router.get ('/:id', async (req, res) => {
+router.get('/:id', async (req, res) => {
   const id = req.params.id;
   try {
-    const hazard = await getHazardById (id);
+    const hazard = await getHazardById(id);
     if (!hazard) {
-      return res.status (404).send ('Invalid hazard id');
+      return res.status(404).send('Invalid hazard id');
     }
-    res.send (hazard);
+    res.send(hazard);
   } catch (error) {
-    debug (error);
-    res.status (500).send (error);
+    debug(error);
+    res.status(500).send(error);
   }
 });
 
